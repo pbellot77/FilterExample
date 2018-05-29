2896fd6d9858f6a886e28e19b40e41e4071a83d5
Patrick <patrickbellot77@gmail.com>
2018-05-29 17:07:12 -0500

    Initial Commit

---
+++ FilterExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FilterExample
+//
+//  Created by Patrick Bellot on 5/29/18.
+//  Copyright © 2018 Polestar Interactive LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
